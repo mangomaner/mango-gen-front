b9d3b34cb348a11a7a26c0c9196b9da458408063
mangomaner <2756477287@qq.com>
2025-10-13 14:58:21 +0800

    fix: 修复回滚消息选择与实际不符的问题

--- src/config/env.ts
+++ src/config/env.ts
@@ -19,7 +19,7 @@ export const getDeployUrl = (deployKey: string) => {
 
 // 获取静态资源预览URL
 export const getStaticPreviewUrl = (codeGenType: string, appId: string) => {
-  const baseUrl = `${STATIC_BASE_URL}/${codeGenType}_${appId}/`
+  const baseUrl = `${STATIC_BASE_URL}/project_${appId}/`
   // 如果是 Vue 项目，浏览地址需要添加 dist 后缀
   if (codeGenType === CodeGenTypeEnum.VUE_PROJECT) {
     return `${baseUrl}dist/index.html`
